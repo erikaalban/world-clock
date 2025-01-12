2aab4851437c6e9722b1ae4f386d682efe77687a
Erika Alban <erikalban12@gmail.com>
2025-01-12 14:02:46 -0500

    displays time of selected city

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function updateTime() {
   let losAngeles = document.querySelector("#los-angeles");
   let losAngelesDateElement = losAngeles.querySelector(".date");
   let losAngelesTimeElement = losAngeles.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los-Angeles");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
@@ -19,6 +19,26 @@ function updateTime() {
     `h:mm:ss [<small>]A[</small>]`
   );
 }
+
+function updateSelectedTime(event) {
+  let city = event.target.value;
+  let cityName = city.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(`${city}`);
+  let cityDisplay = document.querySelector("#selected-city");
+  cityDisplay.innerHTML = `
+    <div class="location-details" id="los-angeles">
+          <div class="details">
+            <h2 class="city">${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>AM</small></div>
+        </div>`;
+}
+
+let dropDown = document.querySelector("#drop-down");
+addEventListener("change", updateSelectedTime);
 
 updateTime();
 setInterval(updateTime, 1000);
